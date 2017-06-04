78b9c0fe31172697018b75b89fd0f4c523b782b6
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-06-03 23:49:28 -0400

    Textfield weird focus  issue

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -4,12 +4,29 @@ import injectTapEventPlugin from "react-tap-event-plugin";
 import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import HorizontalNonLinearStepper from "./Test";
+import TextField from 'material-ui/TextField';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focus: false
+    }
+  }
+
+  inFocus = () => this.setState({focus: true})
+
   render() {
     return (
       <MuiThemeProvider>
-        <HorizontalNonLinearStepper/>
+        <div>
+
+        <RaisedButton label="Focus" onChange={this.inFocus}/>
+        <br />
+        <TextField
+      hintText="Some text" autoFocus={this.state.focus}
+    />
+    </div>
       </MuiThemeProvider>
     );
   }
